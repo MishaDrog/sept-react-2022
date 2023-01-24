9a61db8cacab0010134cb4c387c1db43f4ab6444
MishaDrog <drogmisha@gmail.com>
2023-01-24 23:40:58 +0200

    lesson2 - hw2 (Count) Users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import {Count} from "./components/Count/Count";
+import {Users} from "./components/Users/Users";
 
 
 const App = () => {
     return (
         <div>
-            <Count/>
+            <Users/>
         </div>
     );
 };
---
+++ src/components/Users/Users.js
@@ -0,0 +1,17 @@
+import User from "../User/User";
+import {useState} from "react";
+
+const Users = () => {
+    const [users, setUsers] = useState([])
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+        .then(value => setUsers(value))
+    return (
+        <div>
+            {users.map(user=><User key={user.id} user={user}/>)}
+        </div>
+    );
+};
+
+export {Users};
