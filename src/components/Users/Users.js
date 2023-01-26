0a1076b472065f528d36709ea45934a514ce952b
MishaDrog <drogmisha@gmail.com>
2023-01-26 21:54:00 +0200

    lesson2 - hw2 (Count) Users fetch

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,14 +1,25 @@
 import {User} from "../User/User";
+import {useEffect, useState} from "react";
 
 const Users = () => {
-    let users = [];
+    const [users, setUsers] = useState([]);
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(value => value.json())
-        .then(value => users=value)
+    const [count, setCount] = useState(0);
+
+    useEffect(()=>{
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => setUsers(value))
+
+    },[count])
+
+
 
     return (
+
         <div>
+            <button onClick={()=>setCount(prevState => prevState+1)}>Click </button>
             {users.map(user=> <User key={user.id} user={user}/>)}
         </div>
     );
